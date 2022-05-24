140ae4e489f58c9a95480a4552ed6371b5853583
Matthias <webmaster@pottgames.de>
2022-05-24 03:54:50 +0200

    Apply license
    Signed-off-by: Matthias <webmaster@pottgames.de>

--- lwjgl3/src/main/java/de/pottgames/anemulator/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/de/pottgames/anemulator/lwjgl3/Lwjgl3Launcher.java
@@ -1,3 +1,17 @@
+/**
+ * Anemulator - A Game Boy emulator<br>
+ * Copyright (C) 2022 Matthias Finke
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program. If not, see
+ * <a href="https://www.gnu.org/licenses">https://www.gnu.org/licenses<a/>.
+ */
+
 package de.pottgames.anemulator.lwjgl3;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
